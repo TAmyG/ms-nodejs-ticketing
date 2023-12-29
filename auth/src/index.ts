fe19ec11c44af339156ab76d569263159e9d1e1b
tamy <tamy.vivas@gmail.com>
2023-12-29 12:20:56 -0600

    feat: added error handling and skaffold for GCP

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,33 @@
-import express from "express";
-import { json } from "body-parser";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+// if express-async-errors is not installed you'll need this code
+// app.all('*', async (req, res, next)=>{
+//   next( new NotFoundError());
+// });
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!");
+  console.log('Listening on port 3000!!!!!!!!');
 });
---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../errors/custom-error";
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    console.log('Always get in here')
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }
+
+    res.status(400).send({
+        errors: [{ message: 'Something went wrong' }],
+    });
+};
