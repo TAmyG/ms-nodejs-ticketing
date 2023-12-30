80d9ab470593d049c7d15dd07c97c783695f5607
tamy <tamy.vivas@gmail.com>
2023-12-29 21:17:57 -0600

    feat: added signup functionality and mongo integration

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -7,7 +7,6 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
 ) => {
-    console.log('Always get in here')
     if (err instanceof CustomError) {
         return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
