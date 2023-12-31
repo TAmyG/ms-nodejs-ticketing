3e59e0c98ba6c7f3e4cedccf3de2aaa7e9438c8b
tamy <tamy.vivas@gmail.com>
2023-12-30 18:52:33 -0600

    feat: Add _app default page and signup with basic form

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+// This is the default Component for Next
+const App = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />;
+};
+
+export default App;
