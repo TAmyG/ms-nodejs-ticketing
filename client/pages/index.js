6f52f629a34b6a11f29768f5f8dd8ee214297c6f
tamy <tamy.vivas@gmail.com>
2023-12-30 21:20:50 -0600

    feat: Add signin func and api to ssr current-user requests

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,24 @@
 import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser);
-    return <h1>Landing Page</h1>;
+
+    return currentUser ? (
+        <h1>You are signed in</h1>
+    ) : (
+        <h1>You are NOT signed in</h1>
+    );
 
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
+LandingPage.getInitialProps = async (context) => {
     console.log('I am on the server!');
 
-    if (typeof window === 'undefined') {
-        // We are on the server!
-        // Request should be made to:
-        // http://SERVICENAME.NAMESPACE.svc.cluster.local
-        // Define headers: host and cookie. Host to tell ingress that we need to use the routes of ticketing.dev domain
-        // And cookie because the cookie is in the browser not in server
-        const url = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
-        const { data } = await axios.get(url, {
-            headers: req.headers,
-        });
-        // {currentUser: {}}
-        return data;
-    } else {
-        // We are on the browser!
-        // Request can be made with a base url of ''
-        const { data } = await axios.get('/api/users/currentuser');
-        // {currentUser: {}}
-        return data;
-    }
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
 
-    return response.data;
+    return data;
+
 };
 
 export default LandingPage;
