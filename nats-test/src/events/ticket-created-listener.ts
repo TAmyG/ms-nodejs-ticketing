2c1ea2cf7349f024850d702fb0f0c25daca188d2
tamy <tamy.vivas@gmail.com>
2024-01-04 22:18:36 -0600

    feat: add ts patterns to listener

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,18 @@
+import { Message } from 'node-nats-streaming';
+import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;//define this type to prevent different values or datatypes in this variable
+  queueGroupName = 'payments-service';
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log('Event data!', data);
+
+    console.log(data.id);
+    console.log(data.price);
+
+    msg.ack();
+  }
+}
