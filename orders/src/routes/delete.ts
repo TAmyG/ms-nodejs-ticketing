98baf7544a2e86808256eb54d86e2675da6bfe1f
tamy <tamy.vivas@gmail.com>
2024-01-18 09:26:38 -0600

    feat: Add event for create and cancel order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,13 +1,15 @@
 import { NotAuthorizedError, NotFoundError, requireAuth } from '@tv-tickets/common';
 import express, { Request, Response } from 'express';
 import { Order, OrderStatus } from '../models/order';
+import { natsWrapper } from '../nats-wrapper';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
   const { orderId } = req.params;
 
-  const order = await Order.findById(orderId);
+  const order = await Order.findById(orderId).populate('ticket');
 
   if (!order) {
     throw new NotFoundError();
@@ -19,6 +21,12 @@ router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Res
   await order.save();
 
   // publishing an event saying this was cancelled!
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id,
+    }
+  });
 
   res.status(204).send(order);
 });
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,6 +4,8 @@ import { BadRequestError, NotFoundError, requireAuth, validateRequest } from '@t
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -48,9 +50,41 @@ router.post(
     await order.save();
 
     // Publish and event saying that an order was created
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price
+      }
+    });
 
     res.send({});
   }
 );
+
+router.post('/api/orders/ticketcreation',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+    });
+
+    await ticket.save();
+
+    res.status(201).send(ticket);
+  });
 
 export { router as newOrderRouter };
