cb04fbf0c5a982f81cb808d112133c9285abd775
tamy <tamy.vivas@gmail.com>
2024-02-07 09:34:19 -0600

    feat: expiration event emitted

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -9,7 +9,8 @@ import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+// const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+const EXPIRATION_WINDOW_SECONDS = 10;
 
 router.post(
   '/api/orders',
