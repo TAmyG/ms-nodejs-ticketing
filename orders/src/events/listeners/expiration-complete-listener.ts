c7160d24096f3306c011d3796a67e7f265975cad
tamy <tamy.vivas@gmail.com>
2024-02-18 21:57:49 -0600

    feat: prevent cancelation of paid order

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -20,6 +20,11 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
       throw new Error('Order not found');
     }
 
+    // Preventing cancelation of paid order
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+
     order.set({
       status: OrderStatus.Cancelled,
     });
